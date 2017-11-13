b1317b274919f7b005d8814fdc90ddd49278b0e2
Chris Sciavolino <cds253@cornell.edu>
2017-11-13 18:37:00 -0500

    Tentatively added enum for articles vs photoGalleries and retreive information about media from the attachments array.

--- sun-wordpress.php
+++ sun-wordpress.php
@@ -26,6 +26,8 @@ if ( !class_exists( 'SunAppExtension_Plugin' ) ) {
             add_action( 'rest_api_init', 'SunAppExtension_Plugin::posts_add_category_name' );
             add_action( 'rest_api_init', 'SunAppExtension_Plugin::posts_add_tags_name' );
             add_action( 'rest_api_init', 'SunAppExtension_Plugin::posts_add_comments' );
+            add_action( 'rest_api_init', 'SunAppExtension_Plugin::posts_add_type_enum' );
+            add_action( 'rest_api_init', 'SunAppExtension_Plugin::posts_add_attachments' );
         }
 
         /**
@@ -54,8 +56,11 @@ if ( !class_exists( 'SunAppExtension_Plugin' ) ) {
         public static function posts_add_media_url( $data ) {
             register_rest_field( 'post', 'featured_media_url_string', array(
                 'get_callback' => function ( $post_arr ) {
-                    $media_obj = wp_get_attachment_url( $post_arr["featured_media"] );
-                    return $media_obj;
+                    $media_obj = wp_get_attachment_image_src( 
+                        $post_arr["featured_media"],
+                        "medium-large"
+                    );
+                    return $media_obj[0];
                 }
             ));
         }
@@ -100,6 +105,54 @@ if ( !class_exists( 'SunAppExtension_Plugin' ) ) {
                     return get_comments( array(
                         "post_id" => $post_id
                     ));
+                }
+            ));
+        }
+
+        /**
+         * Returns whether a given post is a photoGallery (this week in photos) or simply
+         * another article to those requesting a post.
+         */
+        public static function posts_add_type_enum( $data ) {
+            register_rest_field( 'post', 'post_type_enum', array(
+                'get_callback' => function ( $post_arr ) {
+                    $post_title = $post_arr["title"]["rendered"];
+                    if ( strpos( strtolower( $post_title ), "this week in photos" ) !== false) {
+                        // photo gallery
+                        return "photoGallery";
+                    } else {
+                        // normal article for now
+                        return "article";
+                    }
+                }
+            ));
+        }
+
+        /**
+         * Return the URLs, captions, and other necessary metadata for all the media 
+         * attachments associated with a single post.
+         */
+        public static function posts_add_attachments( $data ) {
+            register_rest_field( 'post', 'post_attachments_meta', array(
+                'get_callback' => function ( $post_arr ) {
+                    $post_attachments = get_attached_media( "image", $post_arr["id"] );
+                    $media_results = array();
+                    foreach ( $post_attachments as $attachment ) {
+                        $media_result = array();
+                        $media_result["link"] = $attachment->link;
+                        $media_result["caption"] = $attachment->caption;
+                        $media_result["title"] = $attachment->title->rendered;
+
+                        $media_sizes = $attachment->media_details->sizes;
+                        $media_result["full"] = $media_sizes["full"]["source_url"];
+                        $media_result["medium_large"] = $media_sizes["medium_large"]["source_url"];
+
+                        // TODO: Return the author info instead of id
+                        $media_result["author_id"] = $attachment->author;
+
+                        $media_results[ $attachment->ID ] = $media_result;
+                    }
+                    return $media_results;
                 }
             ));
         }
