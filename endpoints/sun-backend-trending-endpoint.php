ea648f4c934f9ab94cde6f4d43246ce7e068f96c
Chris Sciavolino <cds253@cornell.edu>
2018-03-24 18:46:17 -0400

    Tentatively fixed the issue with the /trending endpoint. Should now accurately reflect the trending tags for the top posts

--- endpoints/sun-backend-trending-endpoint.php
+++ endpoints/sun-backend-trending-endpoint.php
@@ -11,17 +11,18 @@ class SunAppExtension_TrendingEndpoint {
      * 10 articles being read in the last few days.
      */
     public static function get_trending_tags() {
+        $cur_file_path = plugin_dir_path( __FILE__ );
+        include_once( $cur_file_path . "../includes/sun-backend-config.php" );
+        include_once( $cur_file_path . "../includes/sun-backend-constants.php" );
 
-        include_once( "../includes/sun-backend-constants.php" );
-        include_once( '../includes/sun-backend-config.php' );
-
+        // request 15 most recent popular posts for 2 days
         $request_url = add_query_arg( array(
             'api_key'   => JETPACK_API_KEY,
-            'blog_uri'  => get_home_url(),
+            'blog_uri'  => 'cornellsun.com',
             'format'    => 'json',
             'days'      => 2,
             'table'     => 'postviews',
-            'limit'     => 10
+            'limit'     => 15
         ), esc_url( 'https://stats.wordpress.com/csv.php' ) );
         $response = wp_remote_get( $request_url );
         $data = json_decode( wp_remote_retrieve_body( $response ) );
@@ -29,30 +30,34 @@ class SunAppExtension_TrendingEndpoint {
         // unsuccessful request, no trending results
         if ( ! is_array( $data ) ) return array( "no trending tags" );
 
-        $results_arr = (array)$data[1]; // TODO: Understand why this is randomly 1
-        $postviews_arr = $results_arr["postviews"];
-        
-        $tag_counts = array();
-        foreach($postviews_arr as $post) {
-            $post_arr = (array)$post;
-            $post_tags = wp_get_post_tags( $post_arr["post_id"], array( 'fields' => 'names') );
-            foreach($post_tags as $tag) {
-                if ( array_key_exists( $tag, $tag_counts ) ) {
-                    $tag_counts[$tag] += 1;
-                } else {
-                    $tag_counts[$tag] = 1;
+        // convert array of objects into 2d of day popular posts array
+        $data = array_map( function ($ele) {
+            return $ele->postviews;
+        }, $data );
+
+        // flatten array and count popular tags
+        $popular_tags = [];
+        foreach( $data as $day_data ) {
+            foreach( $day_data as $pop_post ) {
+                $post_tags = wp_get_post_tags( $pop_post->post_id, array( 'fields' => 'names' ) );
+                foreach( $post_tags as $tag ) {
+                    if ( array_key_exists( $tag, $popular_tags ) ) {
+                        $popular_tags[$tag] += 1;
+                    } else {
+                        $popular_tags[$tag] = 1;
+                    }
                 }
             }
         }
 
-        $num_trending_tags = min( count( $tag_counts ), NUM_TRENDING_TAGS );
+        $num_trending_tags = min( count( $popular_tags ), NUM_TRENDING_TAGS );
 
         // TODO: optimize this to not be as suspect
         $sorted_top_tags = array();
-        while ( count($sorted_top_tags) < $num_trending_tags ) {
+        while ( count( $sorted_top_tags ) < $num_trending_tags ) {
             $cur_most_popular = null;
             $cur_max = 0;
-            foreach($tag_counts as $tag => $count) {
+            foreach( $popular_tags as $tag => $count ) {
                 if ( $count > $cur_max ) {
                     $cur_most_popular = $tag;
                     $cur_max = $count;
@@ -60,7 +65,7 @@ class SunAppExtension_TrendingEndpoint {
             }
 
             array_push( $sorted_top_tags, $cur_most_popular );
-            unset( $tag_counts[$cur_most_popular] );
+            unset( $popular_tags[$cur_most_popular] );
         }
 
         return $sorted_top_tags;
