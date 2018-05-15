5f4e6f99caa22857ee06d56cd61b16e839482453
Mike Fang <mfang2197@gmail.com>
2018-05-15 12:52:19 -0400

    Finished featured-in-category-endpoint

--- endpoints/sun-backend-featured-in-category-endpoint.php
+++ endpoints/sun-backend-featured-in-category-endpoint.php
@@ -1,28 +1,30 @@
 <?php
-  class SunAppExtension_CategoryFeaturedEndpoint {
+  class SunAppExtension_FeaturedInCategoryEndpoint {
 
     /**
     * Initialize and register everything necessary for the
-    * /featured-category-endpoint.php to run properly
+    * /featured_in_category endpoint to run properly
     */
     public static function init() {
         $cur_file_path = plugin_dir_path( __FILE__ );
         include_once( $cur_file_path . "../includes/sun-backend-constant.php" );
 
-        register_rest_route( PLUGIN_ENDPOINT . '/' . PRODUCTION_VERSION, '/featured_in_category', array (
+        register_rest_route( PLUGIN_ENDPOINT . '/' . PRODUCTION_VERSION, '/featured_in_category/(?P<category>[A-Za-z]+)',
+        array(
             'methods' => GET,
-            'callback' => 'SunAppExtension_FeaturedEndpoint::get_category_featured'
+            'callback' => 'SunAppExtension_FeaturedInCategoryEndpoint::get_featured_in_category'
         ));
     }
 
     /**
-     * Return the featured post object corresponding to
-     * a category
+     * Return the featured post object of a specific category
+     * @param String $category_name the category whose featured post will be fetched
+     * @return array $featured_post the currently featured post in the category
      */
-     public static function get_category_featured( $category_name ) {
-        $featured_post = largo_get_featured_posts_in_category( $category_name, 1 );
+     public static function get_featured_in_category( $request ) {
+        $featured_post = largo_get_featured_posts_in_category( $request->get_param( "category" ), 1 );
         $featured_post = get_object_vars( $featured_post[0] );
-        $featured_post["post_info_dict"] = SunAppExtension_PostsFunctions::generate_post_entry( $featured_post["ID"] );
+        $featured_post[ "post_info_dict" ] = SunAppExtension_PostsFunctions::generate_post_entry( $featured_post["ID"] );
         return $featured_post;
      }
   }
