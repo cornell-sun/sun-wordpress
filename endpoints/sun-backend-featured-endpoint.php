f3448c8f9aaed0e402833625bf48ecfc6260cc44
Mike Fang <mfang2197@gmail.com>
2018-05-27 13:19:05 -0700

    Integrated Category Endpoint with Featured Endpoint

--- endpoints/sun-backend-featured-endpoint.php
+++ endpoints/sun-backend-featured-endpoint.php
@@ -3,7 +3,6 @@
  * Handles the /featured endpoint, which returns the featured post prominently posted
  * on the first home page using the largo plugin.
  */
-
 class SunAppExtension_FeaturedEndpoint {
 
     /**
@@ -14,12 +13,16 @@ class SunAppExtension_FeaturedEndpoint {
         $cur_file_path = plugin_dir_path( __FILE__ );
         include_once( $cur_file_path . "../includes/sun-backend-constants.php" );
 
+        register_rest_route( PLUGIN_ENDPOINT . '/' . PRODUCTION_VERSION, '/featured/(?P<category>[A-Za-z ]+)', array(
+            'methods' => 'GET', 
+            'callback' => 'SunAppExtension_FeaturedEndpoint::get_featured_in_category'
+        ));
+
         register_rest_route( PLUGIN_ENDPOINT . '/' . PRODUCTION_VERSION, '/featured', array(
             'methods' => 'GET',
-            'callback' => 'SunAppExtension_FeaturedEndpoint::get_featured_home_post',
+            'callback' => 'SunAppExtension_FeaturedEndpoint::get_featured_home_post'
         ));
     }
-
     /**
      * Return the entire post object corresponding to the featured
      * post on the home page of cornellsun.com.
@@ -29,6 +32,18 @@ class SunAppExtension_FeaturedEndpoint {
         $featured_post["post_info_dict"] = SunAppExtension_PostsFunctions::generate_post_entry( $featured_post["ID"] );
         return $featured_post;
     }
+
+     /**
+     * Return the featured post object of a specific category
+     * @param ArrayAccess $request is an array of parameters which contains the category parameter whose featured post will be fetched
+     * @return array $featured_post the currently featured post in the category
+     */
+    public static function get_featured_in_category( $request ) {
+        $featured_post = largo_get_featured_posts_in_category( $request->get_param( "category" ), 1 );
+        $featured_post = get_object_vars( $featured_post[0] );
+        $featured_post["post_info_dict"] = SunAppExtension_PostsFunctions::generate_post_entry( $featured_post["ID"] );
+        return $featured_post;
+     }
 }
 
 ?>
--- sun-wordpress.php
+++ sun-wordpress.php
@@ -25,7 +25,6 @@ if ( !class_exists( 'SunAppExtension_Plugin' ) ) {
             include_once( 'endpoints/sun-backend-featured-endpoint.php' );
             include_once( 'endpoints/sun-backend-trending-endpoint.php' );
             include_once( 'endpoints/sun-backend-comments-endpoint.php' );
-            include_once( 'endpoints/sun-backend-featured-in-category-endpoint.php' );
             include_once( 'includes/sun-backend-constants.php' );
 
             // add post_info_dict to each of the posts requested
@@ -36,7 +35,6 @@ if ( !class_exists( 'SunAppExtension_Plugin' ) ) {
                 SunAppExtension_TrendingEndpoint::init();
                 SunAppExtension_FeaturedEndpoint::init();
                 SunAppExtension_CommentsEndpoint::init();
-                SunAppExtension_FeaturedInCategoryEndpoint::init();
             } );
 
         }
