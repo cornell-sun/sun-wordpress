af8d243115b134c380f3a1c494c22666b039dc52
Mike Fang <mfang2197@gmail.com>
2021-04-11 18:13:41 -0400

    reformatted endpoint and fixed url decoding

--- endpoints/sun-backend-author-endpoint.php
+++ endpoints/sun-backend-author-endpoint.php
@@ -1,33 +1,33 @@
 <?php
 
 /**
- * Handles the /comments endpoint, which handles retrieving the associated comments
- * for any given article ID.
+ * Handles the /author endpoint, which handles retrieving the author information
+ * and author posts for a given author name in string format
  */
 class SunAppExtension_AuthorEndpoint
 {
 
     /**
-     * Set up and register the /comments endpoint with the rest_api_init function.
+     * Set up and register the /author endpoint with the rest_api_init function.
      */
     public static function init()
     {
         $cur_file_path = plugin_dir_path(__FILE__);
         include_once $cur_file_path . "../includes/sun-backend-constants.php";
 
-        register_rest_route(PLUGIN_ENDPOINT . '/' . PRODUCTION_VERSION, '/author/(?P<author_name>[A-Za-z\+]+)(/(?P<page>\d+))?', array(
+        register_rest_route(PLUGIN_ENDPOINT . '/' . PRODUCTION_VERSION, '/author/(?P<author_name>[A-Za-z0-9\+\%]+)(\?page=(/(?P<page>\d+)))?', array(
             'methods' => 'GET',
             'callback' => 'SunAppExtension_AuthorEndpoint::get_author_info',
         ));
     }
 
     /**
-     * 
+     * Handle a request to the /author endpoint
      */
     public static function get_author_info($request)
     {
         $author_name = $request->get_param("author_name");
-        $author_name = preg_replace("/\+/", ' ', $author_name);
+        $author_name = urldecode($author_name);
         $page = $request->get_param("page");
         $author_info_dict = SunAppExtension_PostsFunctions::get_author_metadata($author_name);
         $author_info_dict["posts"] = self::get_author_posts($author_name, $page);
@@ -35,6 +35,12 @@ class SunAppExtension_AuthorEndpoint
         return $author_info_dict;
     }
 
+    /**
+     * Search the Wordpress Database for all posts associated with author_name.
+     * 
+     * Return an array of post_info_dict containing posts associated with
+     * the target author_name
+     */
     public static function get_author_posts($author_name, $page)
     {
         $author_posts_query = array(
